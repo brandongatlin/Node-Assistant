096729c7855a8c561fe96e14155525daf6f8d0ee
brandongatlin <brandongatlin1981@me.com>
2017-11-19 19:48:57 -0600

    trying to add  functionality to movie function - not working

--- liri.js
+++ liri.js
@@ -75,21 +75,27 @@ function qSpotify() {
 function movie() {
   var queryUrl = "http://www.omdbapi.com/?t=" + flick + "&y=&plot=short&apikey=40e9cece";
 
-  request(queryUrl, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      // console.log(response);
-      console.log("Title: " + JSON.parse(body).Title);
-      console.log("Release Year: " + JSON.parse(body).Year);
-      console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
-      console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
-      console.log("Country of Production: " + JSON.parse(body).Country);
-      console.log("Language(s): " + JSON.parse(body).Language);
-      console.log("Plot: " + JSON.parse(body).Plot);
-      console.log("Actors: " + JSON.parse(body).Actors);
+  if (flick === "") {
+    flick = "mr+nobody";
+  } else {
+    request(queryUrl, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        // console.log(response);
+        console.log("Title: " + JSON.parse(body).Title);
+        console.log("Release Year: " + JSON.parse(body).Year);
+        console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+        console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
+        console.log("Country of Production: " + JSON.parse(body).Country);
+        console.log("Language(s): " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
 
-    }
+      } //end of console logs
 
-  });
+    });
+  } //end of else statement
+
+
 } //end movie function
 
 
