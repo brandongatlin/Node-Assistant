c34e4836b7a0595aec4254c3c6fe8e4b6abffcc5
brandongatlin <brandongatlin1981@me.com>
2017-11-19 19:12:26 -0600

    completed omdb portion of homework

--- liri.js
+++ liri.js
@@ -74,20 +74,25 @@ function qSpotify() {
 } //end spotify function
 
 function movie() {
-  omdb.search(flick, function(err, movies) {
-    if (err) {
-      return console.error(err);
+  var queryUrl = "http://www.omdbapi.com/?t=" + flick + "&y=&plot=short&apikey=40e9cece";
+
+  request(queryUrl, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      // console.log(response);
+      console.log("Title: " + JSON.parse(body).Title);
+      console.log("Release Year: " + JSON.parse(body).Year);
+      console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+      console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
+      console.log("Country of Production: " + JSON.parse(body).Country);
+      console.log("Language(s): " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+
     }
 
-    if (movies.length < 1) {
-      return console.log('No movies were found!');
-    }
-
-    movies.forEach(function(movie) {
-      console.log('%s (%d)', movie.title, movie.year);
-    });
   });
-}
+} //end movie function
+
 
 function doIt() {
 
